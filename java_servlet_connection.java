122c404d63b55c0eb03c4744e4ae5dcef1748f23
yash parmar <parmaryas29@gmail.com>
2026-02-07 23:28:27 +0530

    error solving in java

--- java_servlet_connection.java
+++ java_servlet_connection.java
@@ -1,13 +1,8 @@
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+//@WebServlet("/hello")
 public class java_servlet_connection extends HttpServlet {
 
     @Override
