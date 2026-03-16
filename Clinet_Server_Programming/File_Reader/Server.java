3430e9dd74e85fba1d0349e27d278e80aab9f186
yash parmar <parmaryas29@gmail.com>
2026-03-16 14:45:25 +0530

    client with server

---
+++ Clinet_Server_Programming/File_Reader/Server.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server 
+{
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket ss = new ServerSocket(5000);
+        System.out.println("Server Started ....");
+
+        Socket s = ss.accept();
+        System.out.println("Client connected....");
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        String f_name = dis.readUTF();
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(f_name));
+            String line;
+            String res;
+            while((line = br.readLine()) != null)
+            {
+
+                res = line ;
+                dos.writeUTF(res); 
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        dos.close();
+        dis.close();
+        s.close();
+        ss.close();
+
+    } 
+}
