ade5b17975bb5ef97f21eb240fe0a087a1f6a654
yash parmar <parmaryas29@gmail.com>
2026-03-16 13:36:58 +0530

    add client server socket

---
+++ Clinet_Server_Programming/palindrome/Server.java
@@ -0,0 +1,44 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server 
+{
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket ss = new ServerSocket(5000);
+        System.out.println("Server Started...");
+        
+        Socket s = ss.accept();
+        System.out.println("Client connected....");
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        String str = dis.readUTF();
+
+        StringBuffer new_str = new StringBuffer(str);
+
+        new_str = new_str.reverse();
+        System.out.println(new_str);
+        String result;
+        if(str.equalsIgnoreCase(new_str.toString()))
+        {
+            result = "Your String is palindrome...";   
+        }
+        else
+        {
+            result = "Your String is not palindrome";
+        }
+        dos.writeUTF(result);
+
+        dos.close();
+        dis.close();
+        s.close();
+        ss.close();
+
+            
+    }
+}
---
+++ Clinet_Server_Programming/palindrome/client.java
@@ -0,0 +1,30 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+public class client 
+{
+    public static void main(String[] args) throws IOException
+    {
+        Socket s = new Socket("localhost",5000);
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String :- ");
+        String data = sc.next();
+
+        dos.writeUTF(data);
+
+        String res = dis.readUTF();
+
+        System.out.println(res);
+
+        dis.close();
+        dos.close();
+        s.close();
+
+    }    
+}
