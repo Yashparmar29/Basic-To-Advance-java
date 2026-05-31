2ec1f57effd7e767578eb1c6be605360de69d823
Parmar Yash <parmaryas29@gmail.com>
2026-05-31 23:48:06 +0530

    Remove 'D' grade condition from grading logic

--- example.java
+++ example.java
@@ -28,7 +28,10 @@ class student
         {
             System.out.println("C");
         }
-       
+        else if(marks > 40 && marks < 50)
+        {
+            System.out.println("D");
+        }
         else if(marks > 33 && marks < 40)
         {
             System.out.println("E");
